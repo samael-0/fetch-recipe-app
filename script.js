df6419edcab083ea784b54af9fad8662f74174fb
shrawan <mundushrm@gmail.com>
2024-02-12 21:19:30 +0545

    first commit,incomplete

---
+++ script.js
@@ -0,0 +1,10 @@
+const searchBox = document.querySelector('.searchBox');
+const searchBtn = document.querySelector('.searchBtn');
+const recipieContainer = document.querySelector('.recipie-container');
+
+
+searchBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    console.log("button clicked")
+
+})
